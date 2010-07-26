76defbb641f701bb96e108f8774180bb799afd7f
Andreas Pittrich <lemure@lemure-laptop.(none)>
2010-07-26 22:12:24 +0200

    *bugreport

--- packages/luci/luci-app-bugreport/luasrc/controller/bugreport.lua
+++ packages/luci/luci-app-bugreport/luasrc/controller/bugreport.lua
@@ -17,5 +17,5 @@ $Id$
 module "luci.controller.bugreport"
 
 function index()
-	entry({"admin", "freifunk", "heartbeat"}, form("bugreport"), "Bugreport", 60)
+	entry({"admin", "freifunk", "heartbeat"}, template("bugreport"), "Bugreport", 60)
 end
