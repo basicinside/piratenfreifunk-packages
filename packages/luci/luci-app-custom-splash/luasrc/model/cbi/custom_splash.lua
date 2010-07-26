bcbce7eea445a62184a687d3b7d0dd61c09dced6
robin <robin@bruno.(none)>
2010-07-26 22:45:07 +0200

    remove comments

--- packages/luci/luci-app-custom-splash/luasrc/model/cbi/custom_splash.lua
+++ packages/luci/luci-app-custom-splash/luasrc/model/cbi/custom_splash.lua
@@ -1,18 +1,3 @@
---[[
--------
-LuCI - Lua Configuration Interface
-
-Copyright 2008 Steven Barth <steven@midlink.org>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-	http://www.apache.org/licenses/LICENSE-2.0
-
-$Id$
-]]--
-
 luci.i18n.loadc("freifunk")
 local uci = require "luci.model.uci".cursor()
 local fs = require "luci.fs"
--- packages/luci/luci-app-heartbeat/luasrc/model/cbi/heartbeat.lua
+++ packages/luci/luci-app-heartbeat/luasrc/model/cbi/heartbeat.lua
@@ -1,24 +1,3 @@
---[[
-This file has been modified by
-Andreas Pittrich <andreas.pittrich@web.de>
-in behalf of the german pirate party (Piratenpartei)
-www.piratenpartei.de
-
-Original Disclaimer:
--------
-LuCI - Lua Configuration Interface
-
-Copyright 2008 Steven Barth <steven@midlink.org>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-	http://www.apache.org/licenses/LICENSE-2.0
-
-$Id$
-]]--
-
 luci.i18n.loadc("freifunk")
 local uci = require "luci.model.uci".cursor()
 require("luci.tools.webadmin")
